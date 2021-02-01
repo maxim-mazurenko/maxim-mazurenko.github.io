86584b1d113ff39805cf5ab8ea1166e864444b1d
Maxim <maxim@gmail.com>
2021-02-01 11:36:13 +0400

    projects project_2 1.6

--- project_2/js/script.js
+++ project_2/js/script.js
@@ -93,4 +93,19 @@ $(document).ready(function(){
         return false;
     });
 
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        }
+        else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#up']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
 });
