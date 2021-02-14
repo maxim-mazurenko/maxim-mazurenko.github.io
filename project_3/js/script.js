7e044f0ad2f872b194d781349b67d36954293f0c
Maxim <maxim@gmail.com>
2021-02-14 15:53:28 +0400

    version 2.5 finish_portfolio_desctop

--- project_3/js/script.js
+++ project_3/js/script.js
@@ -11,4 +11,11 @@ closeElem.addEventListener('click', () => {
 });
 overlay.addEventListener('click', () => {
     menu.classList.remove('active')
-});   
+});
+
+const counters = document.querySelectorAll('.skills_progress-subtitle'),
+      lines = document.querySelectorAll('.skills__progress-rectangle-band-active');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
