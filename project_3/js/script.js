c60d94af0fe85099accfc85da25ea901dec8f3e9
Maxim <maxim@gmail.com>
2021-02-22 13:12:25 +0400

    version 3.5 adaptive_me__portfolio_responsive

--- project_3/js/script.js
+++ project_3/js/script.js
@@ -23,3 +23,17 @@ const counters = document.querySelectorAll('.skills_progress-subtitle'),
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$(document).ready(function(){
+        $("#menu").on("click","a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id  = $(this).attr('href'),
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+    
