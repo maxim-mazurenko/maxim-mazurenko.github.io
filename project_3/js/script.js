f0d5a6a79bf72baaba67444f694b31a834dc230d
Maxim <maxim@gmail.com>
2021-02-22 14:04:36 +0400

    version 3.9

--- project_3/js/script.js
+++ project_3/js/script.js
@@ -25,7 +25,7 @@ counters.forEach((item, i) => {
 });
 
 $(document).ready(function(){
-        $("#menu, .pageup").on("click","a", function (event) {
+        $("#menu").on("click","a", function (event) {
     //отменяем стандартную обработку нажатия по ссылке
     event.preventDefault();
     //забираем идентификатор бока с атрибута href
@@ -35,6 +35,18 @@ $(document).ready(function(){
     //анимируем переход на расстояние - top за 1500 мс
     $('body,html').animate({scrollTop: top}, 1500);
     });
+});
+$(document).ready(function(){
+    $(".pageup").on("click","a", function (event) {
+//отменяем стандартную обработку нажатия по ссылке
+event.preventDefault();
+//забираем идентификатор бока с атрибута href
+var id  = $(this).attr('href'),
+//узнаем высоту от начала страницы до блока на который ссылается якорь
+top = $(id).offset().top;
+//анимируем переход на расстояние - top за 1500 мс
+$('body,html').animate({scrollTop: top}, 1500);
+});
 });
 $(window).scroll(function(){
     if ($(this).scrollTop() > 1600) {
